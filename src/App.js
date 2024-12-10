255a41b87d374827e4d6163aeb779ce0d9dc0497
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-12-10 18:44:24 +0000

    card section hard coded

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App (){
     <>
     <Navbar />
     <Hero />
-    <Card />
+    <Card/>
 
     </>
  )
--- src/components/card.js
+++ src/components/card.js
@@ -1,39 +1,62 @@
 import React from "react";
-import CardOne from "../images/c1.jpeg";
+import CardFive from "../images/c5.jpeg";
 import CardTwo from "../images/c2.jpeg";
 import CardThree from "../images/c3.jpeg";
+
 import Star from "../images/Star.jpg";
 
 export default function Card() {
   return (
     <div className="card__section">
-
-
-
-
       <div className="card__container">
-        {/* <div className="status">
+        <div className="status">
           <p className="status__message">SOLD OUT</p>
-        </div> */}
+        </div>
         <img className="cardTwo c_img" src={CardTwo} />
-
 
         <div className="card__para-1">
           <img className="star" src={Star} />
           <p className="star__para">5.0 (6) USA</p>
         </div>
-        <p className="card__para-2" >XVZ Fashion Week</p>
-        <p className="card__para-3" >From %136/ Person</p>
-
-
+        <p className="card__para-2">XVZ Fashion Week</p>
+        <p className="card__para-3"><span className="bold">From %136</span>/ Person</p>
       </div>
 
-
-
-
-
       <div className="card__container">
+        <div className="status">
+          <p className="status__message">SOLD OUT</p>
+        </div>
         <img className="cardThree c_img" src={CardThree} />
+        <div className="card__para-1">
+          <img className="star" src={Star} />
+          <p className="star__para">5.0 (6) USA</p>
+        </div>
+        <p className="card__para-2">XVZ Fashion Week</p>
+        <p className="card__para-3"><span className="bold"> From $136</span>/ Person</p>
+      </div>
+      <div className="card__container">
+        <div className="status">
+          <p className="status__message">SOLD OUT</p>
+        </div>
+        <img className="cardFive c_img" src={CardFive} />
+        <div className="card__para-1">
+          <img className="star" src={Star} />
+          <p className="star__para">5.0 (6) USA</p>
+        </div>
+        <p className="card__para-2">XVZ Fashion Week</p>
+        <p className="card__para-3"><span className="bold">From %136</span>/ Person</p>
+      </div>
+      <div className="card__container">
+        <div className="status">
+          <p className="status__message">SOLD OUT</p>
+        </div>
+        <img className="cardThree c_img" src={CardThree} />
+        <div className="card__para-1">
+          <img className="star" src={Star} />
+          <p className="star__para">5.0 (6) USA</p>
+        </div>
+        <p className="card__para-2">XVZ Fashion Week</p>
+        <p className="card__para-3"><span className="bold">From %136</span>/ Person</p>
       </div>
     </div>
   );
