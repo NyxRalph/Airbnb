be188eeb4c8b37cadd6c2060557b867500aa8720
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-12-10 17:37:34 +0000

    ...

--- src/components/card.js
+++ src/components/card.js
@@ -2,17 +2,36 @@ import React from "react";
 import CardOne from "../images/c1.jpeg";
 import CardTwo from "../images/c2.jpeg";
 import CardThree from "../images/c3.jpeg";
-import CardFour from "../images/c4.jpeg";
+import Star from "../images/Star.jpg";
 
 export default function Card() {
   return (
     <div className="card__section">
+
+
+
+
       <div className="card__container">
-        <div className="status">
+        {/* <div className="status">
           <p className="status__message">SOLD OUT</p>
-        </div>
+        </div> */}
         <img className="cardTwo c_img" src={CardTwo} />
+
+
+        <div className="card__para-1">
+          <img className="star" src={Star} />
+          <p className="star__para">5.0 (6) USA</p>
+        </div>
+        <p className="card__para-2" >XVZ Fashion Week</p>
+        <p className="card__para-3" >From %136/ Person</p>
+
+
       </div>
+
+
+
+
+
       <div className="card__container">
         <img className="cardThree c_img" src={CardThree} />
       </div>
