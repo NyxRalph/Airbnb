3d90d7ffef80b0b33ae93b527c019dfaed373215
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-12-11 14:26:41 +0000

    ...

--- src/components/card.js
+++ src/components/card.js
@@ -1,29 +1,21 @@
 import React from "react";
 
 export default function Card(props) {
-
-
   return (
-    
-
-
-
-      <div className="card__container">
-        <div className="status">
-          <p className="status__message">{props.available}</p>
-        </div>
-        <img className="cardTwo c_img" src={props.img} />
-
-        <div className="card__para-1">
-          <img className="star" src={props.star} />
-          <p className="star__para">{props.para}</p>
-        </div>
-        <p className="card__para-2">{props.para_2}</p>
-        <p className="card__para-3">
-          <span className="bold">{props.para_3}</span>
-          {props.para_4}
-        </p>
+    <div className="card__container">
+      <div className="status">
+        <p className="status__message">{props.available}</p>
       </div>
+      <img className="cardTwo c_img" src={props.img} />
 
+      <div className="card__para-1">
+        <img className="star" src={props.star} />
+        <p className="star__para">{props.rating} {props.review} {props.country}</p>
+      </div>
+      <p className="card__para-2"> {props.title}</p>
+      <p className="card__para-3">
+        <span className="bold"> From {props.price}</span> / Person
+      </p>
+    </div>
   );
 }
