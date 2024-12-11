866a815959e5547b4c6c92c75f4aa72ae5d283b5
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-12-11 12:13:39 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -2,16 +2,57 @@ import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Card from "./components/card";
-import '../src/style.css';
+import "../src/style.css";
+import c2 from "./images/c2.jpeg"
+import c3 from "./images/c3.jpeg"
+import c4 from "./images/c4.jpeg"
+import c5 from "./images/c5.jpeg"
+import star from "./images/Star.jpg"
 
-export default function App (){
- return (
+export default function App() {
+  return (
     <>
-    <Navbar />
-    <Hero />
-    <Card/>
-
+      <Navbar />
+      <Hero />
+       <div className="card__section">
+      <Card 
+      img={c2}
+      available="SOLD OUT" 
+      star={star}
+      para="5.0 (6) USA"
+      para_2="XVZ Fashion Week"
+      para_3=" From %136"
+      para_4="/ Person"
+      />
+      <Card 
+      img={c3}
+      available="SOLD OUT" 
+      star={star}
+      para="5.0 (6) USA"
+      para_2="XVZ Fashion Week"
+      para_3=" From %136"
+      para_4="/ Person"
+      />
+      <Card 
+      img={c5}
+      available="SOLD OUT" 
+      star={star}
+      para="5.0 (6) USA"
+      para_2="XVZ Fashion Week"
+      para_3=" From %136"
+      para_4="/ Person"
+      />
+      <Card 
+      img={c2}
+      available="SOLD OUT" 
+      star={star}
+      para="5.0 (6) USA"
+      para_2="XVZ Fashion Week"
+      para_3=" From %136"
+      para_4="/ Person"
+      />
+     
+           </div>
     </>
- )
- 
+  );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,30 +1,29 @@
 import React from "react";
-import CardFive from "../images/c5.jpeg";
-import CardTwo from "../images/c2.jpeg";
-import CardThree from "../images/c3.jpeg";
 
-import Star from "../images/Star.jpg";
+export default function Card(props) {
 
-export default function Card() {
+
   return (
-    <div className="card__section">
+    
+
+
+
       <div className="card__container">
         <div className="status">
-          <p className="status__message">SOLD OUT</p>
+          <p className="status__message">{props.available}</p>
         </div>
-        <img className="cardTwo c_img" src={CardTwo} />
+        <img className="cardTwo c_img" src={props.img} />
 
         <div className="card__para-1">
-          <img className="star" src={Star} />
-          <p className="star__para">5.0 (6) USA</p>
+          <img className="star" src={props.star} />
+          <p className="star__para">{props.para}</p>
         </div>
-        <p className="card__para-2">XVZ Fashion Week</p>
-        <p className="card__para-3"><span className="bold">From %136</span>/ Person</p>
+        <p className="card__para-2">{props.para_2}</p>
+        <p className="card__para-3">
+          <span className="bold">{props.para_3}</span>
+          {props.para_4}
+        </p>
       </div>
 
-     
-     
-      
-    </div>
   );
 }
